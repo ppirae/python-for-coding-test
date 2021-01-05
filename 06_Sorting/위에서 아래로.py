732163fbaf82475974a9fd4f56b70c0613ecc3e0
Hyeongrae Jo <whgudfo77@naver.com>
2021-01-05 22:31:34 +0900

    Sorting Algorithm

---
+++ 06_Sorting/위에서 아래로.py
@@ -0,0 +1,12 @@
+n = int(input())
+array = []
+
+for i in range(n):
+    array.append(int(input()))
+
+# 파이썬 기본 정렬 라이브러리를 이용해 정렬 수행
+array = sorted(array, reverse=True)
+
+# 정렬이 수행된 결과를 출력
+for i in array:
+    print(i, end = ' ')
